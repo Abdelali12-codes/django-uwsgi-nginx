654db4d3eb29bc4d20488ce6f9672d599ad6c67c
Ubuntu <ubuntu@ip-18-10-2-124.us-west-2.compute.internal>
2024-03-30 23:36:51 +0000

    django startapp

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # django with uwsgi and nginx
+
+## test configuration of nginx
+
+```
+ sudo service nginx configtest
+```
--- djangoapp/settings.py
+++ djangoapp/settings.py
@@ -28,9 +28,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+#STATIC_URL = '/static/'
 # STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static')
+#     os.path.join(BASE_DIR, "static"),
 # ]
+
 
 # Application definition
 
@@ -41,6 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'healthapp',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -119,11 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-#STATIC_URL = 'static/'
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
